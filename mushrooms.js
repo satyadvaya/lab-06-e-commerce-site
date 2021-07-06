b603428e81438d0b06e3b4afb9575b8b72aee1af
satyadvaya <satyadvaya@gmail.com>
2021-07-06 16:24:48 -0700

    added minimal HTML, photo assets, and mushroom objects

---
+++ mushrooms.js
@@ -0,0 +1,60 @@
+const chanterelle = {
+    id: 'chanterelle',
+    name: 'Chanterelle',
+    image: 'chanterelle.jpeg',
+    description: 'Orange, yellow, or white',
+    category: 'fruity aroma',
+    price: 18,
+    cost: 30
+};
+
+const hedgehog = {
+    id: 'hedgehog',
+    name: 'Hedgehog',
+    image: 'hedgehog.jpeg',
+    description: 'Yellow, orange, or brown',
+    category: 'pleasant aroma',
+    price: 27,
+    cost: 45
+};
+
+const matsutake = {
+    id: 'matsutake',
+    name: 'Matsutake',
+    image: 'matsutake.jpeg',
+    description: 'Brownish-yellow',
+    category: 'spicy aroma',
+    price: 36,
+    cost: 60
+};
+
+const lobster = {
+    id: 'lobster',
+    name: 'Lobster',
+    image: 'lobster.jpeg',
+    description: 'Red or orange',
+    category: 'Seafood-like aroma',
+    price: 24,
+    cost: 40
+};
+
+const morrel = {
+    id: 'morrel',
+    name: 'Morrel',
+    image: 'morrel.jpeg',
+    description: 'Brownish-orange',
+    category: 'Earthy aroma',
+    price: 30,
+    cost: 50
+};
+
+
+const truffle = {
+    id: 'truffle',
+    name: 'Truffle',
+    image: 'truffle.jpeg',
+    description: 'White or black',
+    category: 'Musky aroma',
+    price: 33,
+    cost: 55
+};
