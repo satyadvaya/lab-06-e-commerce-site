a9c9ff8ccc39fd85c0c1f3ee5d1c70b3967072a7
satyadvaya <satyadvaya@gmail.com>
2021-07-08 15:00:43 -0700

    added a bare event listener to each of the Add to Cart buttons on the products page

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ for (let mushroom of mushrooms) {
 const addButtons = document.querySelectorAll('.add');
 for (let button of addButtons) {
     button.addEventListener('click', (e) => {
-        console.log("i am clicking button", e.target.value);
-        console.log(typeof(e.target.value));
+        // console.log("i am clicking button", e.target.value);
+        // console.log(typeof(e.target.value));
         // e.target.value is a string, we need our IDs to be numbers
         // addItemtoCart(Number(e.target.value));
     });
